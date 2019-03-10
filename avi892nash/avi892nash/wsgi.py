85ba6c71394cc2242497961f774689ffd643d3c0
Adien7368 <avihsan8922@gmail.com>
2019-03-10 15:58:14 +0530

    django project started

---
+++ avi892nash/avi892nash/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for avi892nash project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avi892nash.settings')
+
+application = get_wsgi_application()
---
+++ lib/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/avinash/Desktop/GitHub/avi892nash.info/lib/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
